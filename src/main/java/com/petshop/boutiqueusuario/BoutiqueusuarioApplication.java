3c4b5286bfd1fa4fbf3eb0d082563bfa344e6bba
Lucca <lucca.lima1984@gmail.com>
2026-07-11 16:07:41 -0300

    Configuração inicial

---
+++ src/main/java/com/petshop/boutiqueusuario/BoutiqueusuarioApplication.java
@@ -0,0 +1,15 @@
+package com.petshop.boutiqueusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueusuarioApplication.class, args);
+
+		System.out.println("Hello world");
+	}
+
+}
