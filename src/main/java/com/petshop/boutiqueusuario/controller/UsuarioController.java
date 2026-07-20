38c4b11a434729868b771aa6aa3a8dc196a6910d
Lucca <lucca.lima1984@gmail.com>
2026-07-20 20:02:36 -0300

    Criado método de login

--- src/main/java/com/petshop/boutiqueusuario/controller/UsuarioController.java
+++ src/main/java/com/petshop/boutiqueusuario/controller/UsuarioController.java
@@ -2,8 +2,12 @@ package com.petshop.boutiqueusuario.controller;
 
 import com.petshop.boutiqueusuario.business.UsuarioService;
 import com.petshop.boutiqueusuario.business.dto.UsuarioDTO;
+import com.petshop.boutiqueusuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,10 +16,21 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvarUsuario(@RequestBody UsuarioDTO usuarioDTO){
         return ResponseEntity.ok(usuarioService.salvarUsuario(usuarioDTO));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                        usuarioDTO.getSenha())
+        );
+        return "Bearer" + jwtUtil.generateToken(authentication.getName());
     }
 
     @GetMapping
